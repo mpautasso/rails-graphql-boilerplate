636cf039e0b78f19182e94a35b4b93ccabbf6937
Mauricio Pautasso <mauricio.pautasso@gmail.com>
2020-05-13 15:18:24 -0300

    Create User and Post GraphQL types

---
+++ app/graphql/types/post_type.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+module Types
+  class PostType < Types::BaseObject
+    field :id, ID, null: false
+    field :title, String, null: false
+    field :body, String, null: false
+    field :user, Types::UserType, null: false
+  end
+end
