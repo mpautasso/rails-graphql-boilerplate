c0620bcf6794b7562997eade6e722311b533e0a6
Mauricio Pautasso <mauricio.pautasso@gmail.com>
2020-05-13 15:17:06 -0300

    Add seed to populate users and posts tables

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -6,3 +7,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+20.times do
+  user = User.create(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    email: Faker::Internet.email
+  )
+
+  10.times do
+    user.posts.create(
+      title: Faker::Lorem.sentence(word_count: 3),
+      body: Faker::Lorem.paragraph(sentence_count: 5)
+    )
+  end
+end
